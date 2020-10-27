96004fb2f8d02427b42d14ea048f09ab8e6cd893
Zerotonine <52973359+Zerotonine@users.noreply.github.com>
2020-10-27 11:19:44 +0100

    init

---
+++ CSH1_Tag1_Aufgabe_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSH1_Tag1_Aufgabe_03
+{
+    class Program
+    {
+        static uint batterieLaufzeit = 5;
+        static string aufgabe = "Ich reiche Bier."; // Bier ist besser als Butter :)
+
+        static bool istBatterieLaufzeitNiedrig()
+        {
+            if (batterieLaufzeit < 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void fuehreAufgabeAus()
+        {
+            if(!istBatterieLaufzeitNiedrig())
+            {
+                Console.WriteLine(aufgabe);
+                batterieLaufzeit--;
+            }
+            else
+            {
+                Console.WriteLine("Ich muss aufgeladen werden!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                fuehreAufgabeAus();
+            }
+            Console.ReadLine();
+        }
+    }
+}
